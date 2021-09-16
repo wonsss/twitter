8365d4b37213ceddc17566d955fca152501398fa
wonsss <59413128+wonsss@users.noreply.github.com>
2021-09-16 11:07:06 +0900

    #5.0 Get My Own Nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,25 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+export default ({ userObj }) => {
   let history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyTweets = async () => {
+    const tweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(tweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyTweets();
+  }, []);
 
   return (
     <>
